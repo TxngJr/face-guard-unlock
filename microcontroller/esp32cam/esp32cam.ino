92c087a8d9d3501c758f95f99d83ca597a735d8d
txngjr <txngjr@gmail.com>
2023-10-05 19:27:44 +0700

    v0.0.2

--- microcontroller/esp32cam/esp32cam.ino
+++ microcontroller/esp32cam/esp32cam.ino
@@ -6,42 +6,40 @@
 
 const char* ssid = "TongTey 2.4G";
 const char* password = "0943712194";
-
-const char* serverName = "192.168.1.45";
-const int serverPort = 25565;
-const String serverPath = "/upload";
+const char* serverName = "swapsjobs.3bbddns.com";
+const int serverPort = 36880;
 
 WiFiClient client;
 
-#define PWDN_GPIO_NUM     32
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM      0
-#define SIOD_GPIO_NUM     26
-#define SIOC_GPIO_NUM     27
-#define Y9_GPIO_NUM       35
-#define Y8_GPIO_NUM       34
-#define Y7_GPIO_NUM       39
-#define Y6_GPIO_NUM       36
-#define Y5_GPIO_NUM       21
-#define Y4_GPIO_NUM       19
-#define Y3_GPIO_NUM       18
-#define Y2_GPIO_NUM        5
-#define VSYNC_GPIO_NUM    25
-#define HREF_GPIO_NUM     23
-#define PCLK_GPIO_NUM     22
+#define PWDN_GPIO_NUM 32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 0
+#define SIOD_GPIO_NUM 26
+#define SIOC_GPIO_NUM 27
+#define Y9_GPIO_NUM 35
+#define Y8_GPIO_NUM 34
+#define Y7_GPIO_NUM 39
+#define Y6_GPIO_NUM 36
+#define Y5_GPIO_NUM 21
+#define Y4_GPIO_NUM 19
+#define Y3_GPIO_NUM 18
+#define Y2_GPIO_NUM 5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM 23
+#define PCLK_GPIO_NUM 22
 
 const int timerInterval = 2000;
 unsigned long previousMillis = 0;
 
 void setup() {
-  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); 
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
   Serial.begin(115200);
 
   WiFi.mode(WIFI_STA);
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
-  WiFi.begin(ssid, password);  
+  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(500);
@@ -81,7 +79,7 @@ void setup() {
     config.jpeg_quality = 12;
     config.fb_count = 1;
   }
-  
+
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
     Serial.printf("Camera init failed with error 0x%x", err);
@@ -89,47 +87,52 @@ void setup() {
     ESP.restart();
   }
 
-  sendPhoto(); 
+  sendPhoto("C");
 }
 
 void loop() {
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= timerInterval) {
-    sendPhoto();
+    sendPhoto("R");
     previousMillis = currentMillis;
   }
 }
 
-void sendPhoto() {
-  camera_fb_t * fb = esp_camera_fb_get();
+void sendPhoto(const String value) {
+  camera_fb_t* fb = esp_camera_fb_get();
   if (!fb) {
     Serial.println("Camera capture failed");
     delay(1000);
     ESP.restart();
   }
-  
+
   Serial.println("Connecting to server: " + String(serverName));
 
   if (client.connect(serverName, serverPort)) {
-    Serial.println("Connection successful!");    
+    Serial.println("Connection successful!");
     String head = "--Boundary\r\nContent-Disposition: form-data; name=\"imageFile\"; filename=\"esp32-cam.jpg\"\r\nContent-Type: image/jpeg\r\n\r\n";
     String tail = "\r\n--Boundary--\r\n";
 
     uint32_t imageLen = fb->len;
     uint32_t extraLen = head.length() + tail.length();
     uint32_t totalLen = imageLen + extraLen;
-  
-    client.println("POST " + serverPath + " HTTP/1.1");
+
+    if (value == "C") {
+      client.println("POST /checker HTTP/1.1");
+    } else if (value == "R") {
+      client.println("POST /uploader HTTP/1.1");
+    }
+
     client.println("Host: " + String(serverName));
     client.println("Content-Length: " + String(totalLen));
     client.println("Content-Type: multipart/form-data; boundary=Boundary");
     client.println();
     client.print(head);
-  
-    uint8_t *fbBuf = fb->buf;
+
+    uint8_t* fbBuf = fb->buf;
     size_t fbLen = fb->len;
     size_t bytesSent = 0;
-    
+
     while (bytesSent < fbLen) {
       size_t bytesToWrite = min(static_cast<size_t>(1024), fbLen - bytesSent);
       client.write(fbBuf + bytesSent, bytesToWrite);
@@ -137,24 +140,24 @@ void sendPhoto() {
     }
 
     client.print(tail);
-    
+
     esp_camera_fb_return(fb);
-    
+
     int timeoutTimer = 1500;
     long startTimer = millis();
-    
+
     while ((startTimer + timeoutTimer) > millis()) {
       Serial.print(".");
-      delay(100);      
+      delay(100);
       while (client.available()) {
         char c = client.read();
         startTimer = millis();
       }
     }
-    
+
     Serial.println();
     client.stop();
   } else {
-    Serial.println("Connection to " + String(serverName) +  " failed.");
+    Serial.println("Connection to " + String(serverName) + " failed.");
   }
 }
